4401a91c7f7dbaeb0cd2822d7da9bee0d2f79a34
Frain <89194392174pp@gmail.com>
2023-10-13 18:33:29 +0300

    refactor(Demo): new hero image

--- IntroToIT/lesson2/Task1/Arcade/Demo.py
+++ IntroToIT/lesson2/Task1/Arcade/Demo.py
@@ -16,10 +16,10 @@ shift = 0
 x_start, y_start = 20, 10
 
 img_file_back =     path.dirname(__file__)+ '/cave.png'
-img_file_hero =     path.dirname(__file__)+ '/m1.png'
-img_file_enemy =    path.dirname(__file__)+ '/enemy.png' 
-img_file_bomb =     path.dirname(__file__)+ '/bomb.png'
-img_file_princess = path.dirname(__file__)+ '/princess.png'
+img_file_hero =     path.dirname(__file__)+ '/flash.png'
+img_file_enemy =    path.dirname(__file__)+ '/batman.png' 
+img_file_bomb =     path.dirname(__file__)+ '/jocker.png'
+img_file_princess = path.dirname(__file__)+ '/pingwin.png'
 FPS = 60
 
 # цвета:
@@ -191,11 +191,11 @@ while run:
             run = False 
         elif event.type == pygame.KEYDOWN: 
             if event.key == pygame.K_LEFT:
-                robin.x_speed = -5 
+                robin.x_speed = -7
             elif event.key == pygame.K_RIGHT:
-                robin.x_speed = 5 
+                robin.x_speed = 7 
             elif event.key == pygame.K_UP:
-                robin.jump(-7)
+                robin.jump(-10)
 
         elif event.type == pygame.KEYUP: 
             if event.key == pygame.K_LEFT:
